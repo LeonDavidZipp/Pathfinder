70b68cba1f02e328e9cf2a4648663c5d9529a16e
Leon David Zipp <leondavidzipp@gmx.de>
2024-12-27 22:40:45 +0100

    adjusted main to function changes in models & algorithm

--- src/main.go
+++ src/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	a "github.com/LeonDavidZipp/Pathfinder/src/algorithm"
 	p "github.com/LeonDavidZipp/Pathfinder/src/parsing"
 )
 
 func main() {
+	// Set up context
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
 	// Parse command line flags
 	inputFile := flag.String("file", "", "Path to maze input file")
 	flag.Parse()
@@ -32,9 +38,11 @@ func main() {
 	}
 
 	// Solve maze
-	solution, err := a.SolveWrapper(mp)
+	solution, err := a.SolveWrapper(ctx, mp)
 	if err != nil {
 		log.Fatalf("Error solving maze: %v", err)
+	} else if solution == nil {
+		log.Fatal("No solution found in time")
 	}
 
 	// Print results
