ab51ebd034fdb1b42a08e44464cbaa882c75f02c
Leon David Zipp <leondavidzipp@gmx.de>
2024-12-27 14:27:14 +0100

    added tests for moving, all passing

--- src/algorithm/algorithm_test.go
+++ src/algorithm/algorithm_test.go
@@ -1,6 +1,7 @@
 package algorithm
 
 import (
+	m "github.com/LeonDavidZipp/Pathfinder/src/models"
 	p "github.com/LeonDavidZipp/Pathfinder/src/parsing"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -26,6 +27,49 @@ var content2 []byte = []byte(
 111111111111111`,
 )
 
+var mp1 *m.Map
+var mp2 *m.Map
+
+func TestMain(m *testing.M) {
+	var err error
+	mp1, err = p.ParseMap(content1)
+	if err != nil {
+		panic(err)
+	}
+
+	mp2, err = p.ParseMap(content2)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMove(t *testing.T) {
+	moveMp, err := p.ParseMap([]byte("1111\n1011\n1S01\n1111"))
+	assert.NoError(t, err)
+	bot := m.NewBot(moveMp.Start)
+	assert.Equal(t, moveMp.Start, bot.Pos)
+
+	bot.Move(m.Right)
+	assert.Equal(t, m.Left, bot.FromDir)
+	assert.Equal(t, moveMp.Rows[2][2], bot.Pos)
+	assert.Equal(t, 1, bot.Steps)
+
+	bot.Move(m.Left)
+	assert.Equal(t, m.Right, bot.FromDir)
+	assert.Equal(t, moveMp.Rows[2][1], bot.Pos)
+	assert.Equal(t, 2, bot.Steps)
+
+	bot.Move(m.Up)
+	assert.Equal(t, m.Down, bot.FromDir)
+	assert.Equal(t, moveMp.Rows[1][1], bot.Pos)
+	assert.Equal(t, 3, bot.Steps)
+
+	bot.Move(m.Down)
+	assert.Equal(t, m.Up, bot.FromDir)
+	assert.Equal(t, moveMp.Rows[2][1], bot.Pos)
+	assert.Equal(t, 4, bot.Steps)
+}
+
 func TestSolveWrapper(t *testing.T) {
 	mp1, err := p.ParseMap(content1)
 	assert.NoError(t, err)
@@ -35,8 +79,10 @@ func TestSolveWrapper(t *testing.T) {
 	sol1, err1 := SolveWrapper(mp1)
 	assert.Nil(t, err1)
 	assert.NotNil(t, sol1)
+	assert.Equal(t, 6, sol1.Steps)
 
 	sol2, err2 := SolveWrapper(mp2)
 	assert.Nil(t, err2)
 	assert.NotNil(t, sol2)
+	assert.Equal(t, 40, sol2.Steps)
 }
